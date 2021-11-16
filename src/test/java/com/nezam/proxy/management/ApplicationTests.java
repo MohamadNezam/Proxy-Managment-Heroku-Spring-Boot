f670882cbb691a3b75cda12b716a9f1f6b8cbfc9
Mohamad Nezam <mohamad.nezam@mail.polimi.it>
2021-11-16 14:45:35 +0200

    spring initializer
    with Spring Web and Thymeleaf  dependencies

---
+++ src/test/java/com/nezam/proxy/management/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nezam.proxy.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

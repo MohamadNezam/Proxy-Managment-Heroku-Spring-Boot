efe4006e888a137c3f7a913f51e2ef2c1340569c
Mohamad Nezam <mohamad.nezam@mail.polimi.it>
2021-11-17 15:16:12 +0200

    delete non used import

--- src/main/java/com/nezam/proxy/management/Application.java
+++ src/main/java/com/nezam/proxy/management/Application.java
@@ -2,9 +2,7 @@ package com.nezam.proxy.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 
 @SpringBootApplication
